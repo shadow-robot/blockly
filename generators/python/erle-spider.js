58d21b9b78caa9c07c2b5130919a67ae01ff2791
Víctor Mayoral Vilches <victor@erlerobot.com>
2015-10-20 12:46:25 +0200

    Stand up in the real robot

--- generators/python/erle-spider.js
+++ generators/python/erle-spider.js
@@ -27,14 +27,56 @@ goog.provide('Blockly.Python.spider');
 goog.require('Blockly.Python');
 
 Blockly.Python['spider_standup'] = function(block) {  
-  // var code = 'print "standing up..."\n';
-  var code = ""
-  code+="import time\n"
-  code+="\n"
-  code+="pub = rospy.Publisher('rosimple', String, queue_size=10)\n"
-  code+="time.sleep(1)\n"
-  code+="pub.publish('Spider spider_standup')\n"
-  return code;
+	// var code = 'print "standing up..."\n';
+	var code = ""
+	// code+="import time\n"
+	// code+="\n"
+	// code+="pub = rospy.Publisher('rosimple', String, queue_size=10)\n"
+	// code+="time.sleep(1)\n"
+	// code+="pub.publish('Spider spider_standup')\n"
+
+	code+="import sys\n"
+	code+="import time\n"
+	code+="from crab_msgs.msg import *\n"
+	code+="from sensor_msgs.msg import Joy\n"
+	code+="\n"	 
+	code+="standup_time=20\n"
+	code+="walking_time=120\n"
+	code+="turning_time=125\n"
+	code+="\n"
+	code+="################\n"
+	code+="## INITIALIZE ##\n"
+	code+="################ \n"
+	code+="pub = rospy.Publisher('/joy', Joy, queue_size=10)\n"
+	code+="msg = Joy()\n"
+	code+="msg.header.stamp = rospy.Time.now()\n"
+	code+="rate = rospy.Rate(10)\n"
+	code+="\n"	 
+	code+="valueAxe = 0.0\n"
+	code+="valueButton = 0\n"
+	code+="for i in range (0, 20):\n"
+	code+="	msg.axes.append(valueAxe)\n"
+	code+="for e in range (0, 17):\n"
+	code+="	msg.buttons.append(valueButton)\n"
+	code+="\n"
+	code+="time.sleep(1)\n"
+	code+="\n"
+	code+="####################\n"
+	code+="## STAND UP 	  ##\n"
+	code+="####################\n"
+	code+="msg.buttons[3] = 1\n"
+	code+="i=0\n"
+	code+="bo=True\n"
+	code+="while not rospy.is_shutdown() and bo:\n"
+	code+="	i=i+1\n"
+	code+="	if (i>standup_time):\n"
+	code+=" 	bo=False\n"
+	code+="		msg.buttons[3] = 0\n"
+	code+="	pub.publish(msg)\n"
+	code+="	rate.sleep()\n"
+	code+="time.sleep(2)\n"
+	
+	return code;
 };
 
 
