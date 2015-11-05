0a9471328ac34cd21b99ce7db8249f23ce0c6a12
Víctor Mayoral Vilches <victor@erlerobot.com>
2015-11-05 17:09:08 +0100

    new fix in the frontend paths

--- core/inject.js
+++ core/inject.js
@@ -151,7 +151,8 @@ Blockly.parseOptions_ = function(options) {
   gridOptions.length = parseFloat(grid['length']) || 1;
   gridOptions.snap = gridOptions.spacing > 0 && !!grid['snap'];
   // var pathToMedia = 'https://blockly-demo.appspot.com/static/media/';
-  var pathToMedia = '10.0.0.1/frontend/pages/assets/';
+  // var pathToMedia = '10.0.0.1/frontend/pages/assets/';
+  var pathToMedia = 'assets/';
   // var pathToMedia = '../pages/assets';
   if (options['media']) {
     pathToMedia = options['media'];
