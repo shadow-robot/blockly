14f32028cfd0cfd8beef6502ced54bf96c76c852
Kirsty Ellis <kirsty@shadowrobot.com>
2016-01-19 16:46:20 +0000

    add function to grasp block

--- blocks/grasp.js
+++ blocks/grasp.js
@@ -1,11 +1,15 @@
-
 Blockly.Blocks['grasp'] = {
   init: function() {
-    this.appendValueInput("NAME")
-        .setCheck("String")
-        .appendField(new Blockly.FieldTextInput("grasp_name"), "NAME");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
+    this.appendDummyInput()
+        .appendField("Grasp")
+        .appendField(new Blockly.FieldDropdown([["Pack", "pack"], ["Open", "open"], ["Victory", "victory"]]), "grasp_name");
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldTextInput("Interpolation time"), "time")
+        .appendField("(s)")
+        .appendField(new Blockly.FieldTextInput("Pause"), "pause")
+        .appendField("(s)");
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
     this.setColour(306);
     this.setTooltip('');
     this.setHelpUrl('http://www.shadowrobot.com/');
--- generators/python/scripts/grasp.py
+++ generators/python/scripts/grasp.py
@@ -1,3 +1,19 @@
 import rospy
 
-rospy.sleep(1.0)
+from sr_robot_commander.sr_hand_commander import SrHandCommander
+rospy.init_node("grasp", anonymous=True)
+
+
+hand_commander = SrHandCommander(hand_parameters=hand_parameters,
+                                 hand_serial=hand_serial)
+
+trajectory = [
+    {
+        'name': dropdown_grasp,
+        'interpolate_time': time,
+        'pause_time': pause
+    }
+]
+
+# Run trajectory via moveit
+hand_commander.run_named_trajectory(trajectory)
