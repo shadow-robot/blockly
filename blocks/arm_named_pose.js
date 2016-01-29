aa38d7e70dcaecec34cdde7a631a2327acf12f85
Kirsty Ellis <kirsty@shadowrobot.com>
2016-01-29 14:52:40 +0000

    demo

--- blocks/arm_named_pose.js
+++ blocks/arm_named_pose.js
@@ -2,17 +2,17 @@ Blockly.Blocks['arm_named_pose'] = {
   init: function() {
     this.appendDummyInput()
         .appendField("Move arm to named pose:")
-        .appendField(new Blockly.FieldDropdown([["Home", "home"], ["Up", "up"]]), "arm_pose_name");
+        .appendField(new Blockly.FieldDropdown([["Home", "ra_home"], ["Up", "ra_up"], ["Start pose", "start_pose"], ["Face tools", "face_tools_pose"], ["Wrench approach 1", "approach_wrench_2"], ["Wrench approach 2", "approach_wrench_3"], ["Wrench approach 3", "approach_wrench_4"], ["Wrench approach 4", "approach_wrench_5"], ["Lift wrench", "lift_wrench"], ["Remove wrench from hook", "move_fron_wrench"], ["Screwdriver pregrasp 1", "screwdriver_pregrasp"], ["Screwdriver pregrasp 2", "screwdriver_pregrasp_2"], ["Screwdriver pregrasp 3", "screwdriver_pregrasp_3"], ["Lift screwdriver", "screwdriver_up"], ["Remove screwdriver from hook", "screwdriver_free"], ["Place object", "wrench_down_1"], ["Drop object", "wrench_down_drop"]]), "arm_pose_name");
     this.appendDummyInput()
         .appendField("Interpolation time: ")
         .appendField(new Blockly.FieldTextInput("3"), "time")
         .appendField("s")
         .appendField("Pause: ")
-        .appendField(new Blockly.FieldTextInput("1"), "pause")
+        .appendField(new Blockly.FieldTextInput("0"), "pause")
         .appendField("s");
     this.setPreviousStatement(true);
     this.setNextStatement(true);
-    this.setColour(306);
+    this.setColour(230);
     this.setTooltip('');
     this.setHelpUrl('http://www.shadowrobot.com/');
   }
--- blocks/grasp.js
+++ blocks/grasp.js
@@ -2,7 +2,7 @@ Blockly.Blocks['grasp'] = {
   init: function() {
     this.appendDummyInput()
         .appendField("Grasp")
-        .appendField(new Blockly.FieldDropdown([["Pack", "pack"], ["Open", "open"], ["Fingers packed, open thumb", "fingers_pack_thumb_open"]]), "grasp_name");
+        .appendField(new Blockly.FieldDropdown([["Pack", "pack"], ["Open", "open"], ["Fingers packed, open thumb", "fingers_pack_thumb_open"], ["Start pose", "start_pose"], ["Open hand for wrench", "approach_wrench_5"], ["Grasp wrench", "grasp_wrench_script_pose"], ["Screwdriver pregrasp 1", "screwdriver_pregrasp"], ["Screwdriver pregrasp 2", "screwdriver_pregrasp_2"], ["Screwdriver pregrasp 3", "screwdriver_pregrasp_3"], ["Grasp screwdriver", "screwdriver_grasp"], ["Place object", "wrench_down_1"], ["Drop object", "wrench_down_drop"]]), "grasp_name");
     this.appendDummyInput()
         .appendField("Interpolation time: ")
         .appendField(new Blockly.FieldTextInput("3"), "time")
