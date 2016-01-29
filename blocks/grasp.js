1f80d8a32c1210a56020c3c24784e0b26553f80c
Kirsty Ellis <kirsty@shadowrobot.com>
2016-01-29 16:24:44 +0000

    washer

--- blocks/grasp.js
+++ blocks/grasp.js
@@ -2,13 +2,13 @@ Blockly.Blocks['grasp'] = {
   init: function() {
     this.appendDummyInput()
         .appendField("Grasp")
-        .appendField(new Blockly.FieldDropdown([["Pack", "pack"], ["Open", "open"], ["Fingers packed, open thumb", "fingers_pack_thumb_open"], ["Start pose", "start_pose"], ["Open hand for wrench", "approach_wrench_5"], ["Grasp wrench", "grasp_wrench_script_pose"], ["Screwdriver pregrasp 1", "screwdriver_pregrasp"], ["Screwdriver pregrasp 2", "screwdriver_pregrasp_2"], ["Screwdriver pregrasp 3", "screwdriver_pregrasp_3"], ["Grasp screwdriver", "screwdriver_grasp"], ["Place object", "wrench_down_1"], ["Drop object", "wrench_down_drop"]]), "grasp_name");
+        .appendField(new Blockly.FieldDropdown([["Pack", "pack"], ["Open", "open"], ["Fingers packed, open thumb", "fingers_pack_thumb_open"], ["Start pose", "start_pose"], ["Open hand for wrench", "approach_wrench_5"], ["Grasp wrench", "grasp_wrench_script_pose"], ["Screwdriver pregrasp 1", "screwdriver_pregrasp"], ["Screwdriver pregrasp 2", "screwdriver_pregrasp_2"], ["Screwdriver pregrasp 3", "screwdriver_pregrasp_3"], ["Grasp screwdriver", "screwdriver_grasp"], ["Washer pregrasp 1", "washer_pregrasp_1"], ["Washer pregrasp 2", "washer_pregrasp_2"], ["Grasp washer", "washer_grasp"], ["Place object", "wrench_down_1"], ["Drop object", "wrench_down_drop"]]), "grasp_name");
     this.appendDummyInput()
         .appendField("Interpolation time: ")
         .appendField(new Blockly.FieldTextInput("3"), "time")
         .appendField("s")
         .appendField("Pause: ")
-        .appendField(new Blockly.FieldTextInput("1"), "pause")
+        .appendField(new Blockly.FieldTextInput("0"), "pause")
         .appendField("s");
     this.setPreviousStatement(true);
     this.setNextStatement(true);
