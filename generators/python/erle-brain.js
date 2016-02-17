1d379c354954f91092dfc50f6f3e582af9402198
Nestor94Gonzalez <nestor94gonzalez@gmail.com>
2016-02-17 12:36:41 +0000

    erle-brain statusleds update

---
+++ generators/python/erle-brain.js
@@ -0,0 +1,88 @@
+/**
+ * @license
+ *
+ * Copyright 2015 Erle Robotics
+ * http://erlerobotics.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Blocks for Erle-Spider.
+ * @author victor@erlerobot.com (Víctor Mayoral Vilches)
+ */
+'use strict';
+
+goog.provide('Blockly.Python.brain');
+goog.require('Blockly.Python');
+
+Blockly.Python['turn_on_blue_led'] = function(block) {
+        var blue_led = block.getFieldValue('BLUE_LED');
+	var code = "";
+	code+="#!/usr/bin/python\n"
+	code+="import rospy\n"
+	code+="import time\n"
+	code+="from std_msgs.msg import String\n"
+	code+="\n"
+	code+="def talker():\n"
+	code+="  pub = rospy.Publisher('/statusleds', String, queue_size=10)\n"
+	code+="  rate = rospy.Rate(10)\n"
+	code+="  start = time.time()\n"
+	code+="  flag=True #time flag\n"
+        code+="  led='"+blue_led.toString()+"'\n"
+	code+="  if (led == 'TRUE'):\n"
+        code+="    msg = 'blue'\n"
+        code+="  else:\n"
+        code+="    msg = 'blue_off'\n"
+	code+="  while not rospy.is_shutdown() and flag:\n"
+	code+="    sample_time=time.time()\n"
+  	code+="    if ((sample_time - start) > 1):\n"
+	code+="      flag=False\n"
+	code+="    pub.publish(msg)\n"
+	code+="    rate.sleep()\n"
+	code+="if __name__ == '__main__':\n"
+	code+="  talker()\n"	
+        return code;
+
+};
+
+Blockly.Python['turn_on_orange_led'] = function(block) {
+        var orange_led = block.getFieldValue('ORANGE_LED');
+        var code = "";
+        code+="#!/usr/bin/python\n"
+        code+="import rospy\n"
+        code+="import time\n"
+        code+="from std_msgs.msg import String\n"
+        code+="\n"
+        code+="def talker():\n"
+        code+="  pub = rospy.Publisher('/statusleds', String, queue_size=10)\n"
+        code+="  rate = rospy.Rate(10)\n"
+        code+="  start = time.time()\n"
+        code+="  flag=True #time flag\n"
+        code+="  led='"+orange_led.toString()+"'\n"
+        code+="  if (led == 'TRUE'):\n"
+        code+="    msg = 'orange'\n"
+        code+="  else:\n"
+        code+="    msg = 'orange_off'\n"
+        code+="  while not rospy.is_shutdown() and flag:\n"
+        code+="    sample_time=time.time()\n"
+        code+="    if ((sample_time - start) > 1):\n"
+        code+="      flag=False\n"
+        code+="    pub.publish(msg)\n"
+        code+="    rate.sleep()\n"
+        code+="if __name__ == '__main__':\n"
+        code+="  talker()\n"
+        return code;
+
+};
+
