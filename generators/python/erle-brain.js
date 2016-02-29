1efa53d4572f8870280138dbc5730c3901716938
Alejandro Hernández Cordero <ahcorde@gmail.com>
2016-02-29 15:52:55 +0100

    Add erle-brain generators (get_laser and take_a_picture)

--- generators/python/erle-brain.js
+++ generators/python/erle-brain.js
@@ -20,18 +20,52 @@
 /**
  * @fileoverview Blocks for Erle-Spider.
  * @author victor@erlerobot.com (Víctor Mayoral Vilches)
+ * @author ahcorde@erlerobot.com (Alejandro Hernández Cordero)
  */
 'use strict';
 
 goog.provide('Blockly.Python.brain');
 goog.require('Blockly.Python');
 
+Blockly.Python['get_laser'] = function(block) {
+
+    var varName = Blockly.Python.valueToCode(block, 'laser', Blockly.Python.ORDER_ATOMIC);
+    var code = "";
+    code+="import rospy\n"
+    code+="import subprocess\n"
+    code+="import rosnode\n"
+    code+="from sensor_msgs.msg import Range\n"
+    code+="ros_nodes = rosnode.get_node_names()\n"
+    code+="if not '/lrm30_node' in ros_nodes:\n"
+    code+="  command='/home/erle/spider_ws/install_isolated/lib/lrm30_ros/lrm30'\n"
+    code+="  process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)\n"
+    code+="msg_laser = rospy.wait_for_message('/lrm30_data', Range, timeout=1)\n"
+    return code + varName + " = msg_laser.range\n"
+};
+
+Blockly.Python['take_a_picture'] = function(block) {
+
+    var code = "";
+    code+="import rospy\n"
+    code+="import subprocess\n"
+    code+="import rosnode\n"
+    code+="ros_nodes = rosnode.get_node_names()\n"
+    code+="if '/raspicam_node' in ros_nodes:\n"
+    code+="  command='rosservice call /camera/start_capture'\n"
+    code+="  process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)\n"
+    code+="command='python2 /home/erle/take_a_picture.py'\n"
+    code+="process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)\n"
+    return code;
+};
+
 Blockly.Python['turn_on_blue_led'] = function(block) {
-        var blue_led = block.getFieldValue('BLUE_LED');
+	var blue_led = block.getFieldValue('BLUE_LED');
 	var code = "";
 	code+="#!/usr/bin/python\n"
 	code+="import rospy\n"
 	code+="import time\n"
+	code+="import rosnode\n"
+	code+="import subprocess\n"
 	code+="from std_msgs.msg import String\n"
 	code+="\n"
 	code+="def talker():\n"
@@ -39,50 +73,54 @@ Blockly.Python['turn_on_blue_led'] = function(block) {
 	code+="  rate = rospy.Rate(10)\n"
 	code+="  start = time.time()\n"
 	code+="  flag=True #time flag\n"
-        code+="  led='"+blue_led.toString()+"'\n"
+	code+="  led='"+blue_led.toString()+"'\n"
 	code+="  if (led == 'TRUE'):\n"
-        code+="    msg = 'blue'\n"
-        code+="  else:\n"
-        code+="    msg = 'blue_off'\n"
+	code+="    msg = 'blue'\n"
+	code+="  else:\n"
+	code+="    msg = 'blue_off'\n"
 	code+="  while not rospy.is_shutdown() and flag:\n"
 	code+="    sample_time=time.time()\n"
-  	code+="    if ((sample_time - start) > 1):\n"
+	code+="    if ((sample_time - start) > 1):\n"
 	code+="      flag=False\n"
 	code+="    pub.publish(msg)\n"
 	code+="    rate.sleep()\n"
 	code+="if __name__ == '__main__':\n"
+	code+="  ros_nodes = rosnode.get_node_names()\n"
+	code+="  if not '/erle_statusleds' in ros_nodes:\n"
+	code+="    command='python /home/erle/spider_ws/src/ros_erle_statusled/scripts/statusleds.py'\n"
+	code+="    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)\n"
 	code+="  talker()\n"	
-        return code;
+	return code;
 
 };
 
 Blockly.Python['turn_on_orange_led'] = function(block) {
-        var orange_led = block.getFieldValue('ORANGE_LED');
-        var code = "";
-        code+="#!/usr/bin/python\n"
-        code+="import rospy\n"
-        code+="import time\n"
-        code+="from std_msgs.msg import String\n"
-        code+="\n"
-        code+="def talker():\n"
-        code+="  pub = rospy.Publisher('/statusleds', String, queue_size=10)\n"
-        code+="  rate = rospy.Rate(10)\n"
-        code+="  start = time.time()\n"
-        code+="  flag=True #time flag\n"
-        code+="  led='"+orange_led.toString()+"'\n"
-        code+="  if (led == 'TRUE'):\n"
-        code+="    msg = 'orange'\n"
-        code+="  else:\n"
-        code+="    msg = 'orange_off'\n"
-        code+="  while not rospy.is_shutdown() and flag:\n"
-        code+="    sample_time=time.time()\n"
-        code+="    if ((sample_time - start) > 1):\n"
-        code+="      flag=False\n"
-        code+="    pub.publish(msg)\n"
-        code+="    rate.sleep()\n"
-        code+="if __name__ == '__main__':\n"
-        code+="  talker()\n"
-        return code;
+    var orange_led = block.getFieldValue('ORANGE_LED');
+    var code = "";
+    code+="#!/usr/bin/python\n"
+    code+="import rospy\n"
+    code+="import time\n"
+    code+="from std_msgs.msg import String\n"
+    code+="\n"
+    code+="def talker():\n"
+    code+="  pub = rospy.Publisher('/statusleds', String, queue_size=10)\n"
+    code+="  rate = rospy.Rate(10)\n"
+    code+="  start = time.time()\n"
+    code+="  flag=True #time flag\n"
+    code+="  led='"+orange_led.toString()+"'\n"
+    code+="  if (led == 'TRUE'):\n"
+    code+="    msg = 'orange'\n"
+    code+="  else:\n"
+    code+="    msg = 'orange_off'\n"
+    code+="  while not rospy.is_shutdown() and flag:\n"
+    code+="    sample_time=time.time()\n"
+    code+="    if ((sample_time - start) > 1):\n"
+    code+="      flag=False\n"
+    code+="    pub.publish(msg)\n"
+    code+="    rate.sleep()\n"
+    code+="if __name__ == '__main__':\n"
+    code+="  talker()\n"
+    return code;
 
 };
 
